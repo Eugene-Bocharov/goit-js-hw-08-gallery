f81a73b5872797f5358a9e7041164e7f4ffa0c5f
Eugene Bocharov <bocahrovwork12@gmail.com>
2021-11-21 12:38:54 +0200

    finaly

--- js/index.js
+++ js/index.js
@@ -1,70 +1,73 @@
 import galleryItems from "./app.js";
 
-// console.log(galleryItems);
+//  adding additional variables
 
-const galleryplace = {
-  ul: document.querySelector(".gallery"),
-};
+const ul = document.querySelector(".gallery");
+const lightbox = document.querySelector(".lightbox");
+const overlay = document.querySelector(".lightbox__overlay");
+const close_button = document.querySelector(".lightbox__button");
 
-// console.log(galleryplace.ul);
-
-function addig_function(galleryItems) {
+function addig_func(galleryItems) {
   galleryItems.forEach((galleryItem) => {
     // adding items of gallery
-
-    const li = document.createElement("li");
-    li.className = "gallery__item";
-    const img = document.createElement("img");
-    img.src = galleryItem.preview;
-    img.className = "gallery__image";
-    img.alt = galleryItem.description;
-    const big_img = document.createElement("a");
-    big_img.className = "gallery__link";
-    // big_img.href = galleryItem.original;
-    big_img.appendChild(img);
-    li.appendChild(big_img);
-    // console.log(li);
-    galleryplace.ul.appendChild(li);
-
-    //  adding additional variables
-
-    const lightbox = document.querySelector(".lightbox");
-    const overlay = document.querySelector(".lightbox__overlay");
-    const close_button = document.querySelector(".lightbox__button");
-
-    // open modal part
-
-    img.addEventListener("click", (event) => {
-      img.src = galleryItem.original;
-      //   img.classList.add("lightbox__image");
-      img.classList.replace("gallery__image", "lightbox__image");
-      lightbox.classList.replace("lightbox", "lightbox.is-open");
-    });
-
-    // closing modal
-
-    close_button.addEventListener("click", (event) => {
-      close_lightbox();
-    });
-
-    overlay.addEventListener("click", (event) => {
-      close_lightbox();
-    });
-
-
-    document.addEventListener('keydown', function(event){
-        if(event.key === "Escape"){
-            close_lightbox()
-        }
-    });
-
-    function close_lightbox() {
-      img.src = galleryItem.preview;
-      img.classList.replace("lightbox__image", "gallery__image");
-      lightbox.classList.replace("lightbox.is-open", "lightbox");
-    }
+    creating_func(galleryItem);
   });
 }
 
+function creating_func(galleryItem) {
+  const li = document.createElement("li");
+  li.className = "gallery__item";
+  const img = document.createElement("img");
 
-addig_function(galleryItems);
+  img.setAttribute("data-preview", galleryItem.preview);
+  img.setAttribute("data-original", galleryItem.original);
+
+  img.src = galleryItem.preview;
+  img.className = "gallery__image";
+  img.alt = galleryItem.description;
+  const big_img = document.createElement("a");
+  big_img.className = "gallery__link";
+  big_img.appendChild(img);
+  li.appendChild(big_img);
+  ul.appendChild(li);
+}
+
+ul.addEventListener("click", (event) => {
+  const origin = event.target.getAttribute("data-original");
+  event.target.src = origin;
+
+  const item = event.target;
+
+  event.target.classList.add("lightbox__image");
+  event.target.classList.replace("gallery__image", "lightbox__image");
+  lightbox.classList.replace("lightbox", "lightbox.is-open");
+
+  close_modal(item);
+});
+
+addig_func(galleryItems);
+
+function close_modal(item) {
+  // closing modal
+
+  close_button.addEventListener("click", (event) => {
+    close_lightbox(item);
+  });
+
+  overlay.addEventListener("click", (event) => {
+    close_lightbox(item);
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      close_lightbox(item);
+    }
+  });
+
+  function close_lightbox() {
+    const preview = item.getAttribute("data-preview");
+    item.src = preview;
+    item.classList.replace("lightbox__image", "gallery__image");
+    lightbox.classList.replace("lightbox.is-open", "lightbox");
+  }
+}
